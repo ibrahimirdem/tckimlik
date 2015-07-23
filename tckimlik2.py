0465565b9dcc02979972c44d98c30f1eefdf2c0b
ibrahimirdem <irdem007@gmail.com>
2015-07-24 01:43:50 +0300

    Create tckimlik2.py

---
+++ tckimlik2.py
@@ -0,0 +1,12 @@
+tcno = raw_input("TC Kimlik No Ilk 9 Hanesini Giriniz:")
+a, b, toplamDokuz = 0, 0, 0
+for i in range(9):
+    toplamDokuz = toplamDokuz+int(tcno[i])
+    if i%2 == 0:
+        a = a+7*int(tcno[i])
+    elif i%2 == 1:
+        b = b+int(tcno[i])
+    if i == 8:
+        haneOn = (a-b)%10
+        haneOnBir = (haneOn+toplamDokuz)%10
+        print tcno+str(haneOn)+str(haneOnBir)
